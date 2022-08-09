9057e816d7829fd4f4c7256ad835f8081e049790
russelsy <russelgabrielsy@gmail.com>
2022-08-09 15:21:45 -0400

    finished 4.4

---
+++ blog_list/utils/list_helper.js
@@ -0,0 +1,18 @@
+const dummy = blogs => {
+  return 1
+}
+
+const totalLikes = blogs => {
+  let total = 0
+
+  blogs.forEach(blog => {
+    total += blog.likes
+  })
+
+  return total
+}
+
+module.exports = {
+  dummy,
+  totalLikes
+}
